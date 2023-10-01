3013c3426915c7330b99b3ba12015f77676d5352
Malnormalulo <malnormalulo@users.noreply.github.com>
2023-10-01 19:47:56 -0400

    Cleanup, formatting, style

--- Source/Patch_SanguophageUtility_InSunlight.cs
+++ Source/Patch_SanguophageUtility_InSunlight.cs
@@ -1,30 +1,35 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
-using Dubs_Skylight;
-using HarmonyLib;
-using RimWorld;
-using Verse;
+using OpCodes = System.Reflection.Emit.OpCodes;
+
+using AccessTools = HarmonyLib.AccessTools;
+using CodeInstruction = HarmonyLib.CodeInstruction;
+using HarmonyPatch = HarmonyLib.HarmonyPatch;
+
+using MapComp_Skylights = Dubs_Skylight.MapComp_Skylights;
 
 namespace com.malnormalulo.SkylightSunlight
 {
-	[HarmonyPatch(typeof(SanguophageUtility), "InSunlight")]
+	[HarmonyPatch(typeof(RimWorld.SanguophageUtility), "InSunlight")]
 	internal static class Patch_SanguophageUtility_InSunlight
 	{
 		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
 		{
 			foreach (var instruction in instructions)
 			{
+				// Intercept and replace the call to Verse.RoofGrid.Roofed
 				if (instruction.opcode == OpCodes.Callvirt
-					&& ((MethodInfo)instruction.operand).DeclaringType == typeof(RoofGrid)
+					&& ((MethodInfo)instruction.operand).DeclaringType == typeof(Verse.RoofGrid)
 					&& ((MethodInfo)instruction.operand).Name == "Roofed")
 				{
 					var method = AccessTools.Method(
 						typeof(Patch_SanguophageUtility_InSunlight),
 						nameof(Patch_SanguophageUtility_InSunlight.RoofedWithoutSkylight)
 					);
+					// The RoofGrid and the IntVec3 are already on the evaluation stack.
+					// Put the argument at index 1 (the Map) on the evaluation stack.
 					yield return new CodeInstruction(OpCodes.Ldarg_1);
+					// Call RoofedWithoutSky with all three values on the evaluation stack.
 					yield return new CodeInstruction(OpCodes.Callvirt, method);
 				}
 				else
@@ -34,7 +39,7 @@ namespace com.malnormalulo.SkylightSunlight
 			}
 		}
 
-		public static bool RoofedWithoutSkylight(this RoofGrid rg, IntVec3 cell, Map map)
+		public static bool RoofedWithoutSkylight(this Verse.RoofGrid rg, Verse.IntVec3 cell, Verse.Map map)
 		{
 			var skylightComp = MapComp_Skylights.LightComps[map.uniqueID];
 			var isSkylight = skylightComp.SkylightGrid[map.cellIndices.CellToIndex(cell)];
