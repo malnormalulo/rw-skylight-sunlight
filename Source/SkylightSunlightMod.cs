e5a2e4cd0f502b00f0218cbfbf39f28c0d4bc0dd
Malnormalulo <malnormalulo@users.noreply.github.com>
2023-10-01 13:50:24 -0400

    Initial commit

---
+++ Source/SkylightSunlightMod.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+
+namespace com.malnormalulo.SkylightSunlight
+{
+    [StaticConstructorOnStartup]
+    public class SkylightSunlightMod
+    {
+        static SkylightSunlightMod()
+        {
+            Log.Message("Hello, World!");
+        }
+    }
+}
